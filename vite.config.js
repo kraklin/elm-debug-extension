e0037c434fd15ec778732c26af016ea3237c485c
Tomáš  Látal <latal.tomas@gmail.com>
2023-08-19 21:51:50 +0200

    Zip the build artifacts

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import webExtension, { readJsonFile } from "vite-plugin-web-extension";
 import elmPlugin from "vite-plugin-elm"
+import zipPlugin from "vite-plugin-zip-pack"
 
 const browser = process.env.TARGET || "chrome";
+const pkg = readJsonFile("package.json");
+const outputDir = `dist/${browser}`;
+const bundleExtension = browser === 'firefox' ? '.xpi' : '.zip'
+const bundleFileName = `${pkg.name}-${pkg.version}-${browser}${bundleExtension}`;
 
 function generateManifest() {
   const manifest = readJsonFile("manifest.json");
-  const pkg = readJsonFile("package.json");
   return {
     description: pkg.description,
     version: pkg.version,
@@ -16,7 +20,8 @@ function generateManifest() {
 
 export default defineConfig({
   build: {
-    minify: false
+    minify: false,
+    outDir: outputDir
   },
   plugins: [
     elmPlugin.plugin(),
@@ -25,5 +30,8 @@ export default defineConfig({
       browser: browser,
       watchFilePaths: ["package.json", "manifest.json"],
     }),
+    zipPlugin({inDir: outputDir,
+    outDir: 'dist/bundles',
+    outFileName: bundleFileName})
   ],
 });
